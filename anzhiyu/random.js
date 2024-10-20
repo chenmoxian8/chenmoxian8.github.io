e6a6d4e2bb90fdbb9478a3055c645d46597e86a5
chenmoxian8 <550201358@qq.com>
2024-10-20 16:55:28 +0800

    Site updated: 2024-10-20 16:55:27

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/19/新主题的博文/","2024/10/19/一篇博文/","2024/10/19/hello-world/"];function toRandomPost(){
+var posts=["2024/10/20/凌冬将至/","2024/10/19/新主题的博文/","2024/10/19/一篇博文/","2024/10/19/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
